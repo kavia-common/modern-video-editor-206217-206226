28b38cb3678d472d5e8a86bacbbad4817bac6e03
Kavia AI <production@kavia.ai>
2026-01-28 04:08:13 +0000

    Checkpoint 1 - cg98be8a5f

---
+++ video_editor_frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'video_editor_frontend is being generated';
+}
